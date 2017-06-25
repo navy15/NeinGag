3c580ddbda9743501f4bd057e44a4d3a44f0a5fd
Yvan <yvan.betremieux@orange.fr>
2017-06-25 16:02:37 +0200

    api done

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
-NeinGaG
+Marche à suivre : 
 =======
 
-A Symfony project created on May 22, 2017, 11:44 am.
+Lancer docker : 
+> docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:latest
 
-RAF : 
+Créer la base de données : 
+> php bin/console doctrine:database:create
 
-	Recherche par nom
-	rajouter le nom d'utilisateur sur le post
-	Mise en forme des posts
-	Enlever boutons en fonction de connecté/pas conecté
-	Commentaire / Note sur les posts
-	Commentaire code PSR 4
-	Afficher un post
-	(Tri par note) 
+Créer la structure de la base de données :
+> php bin/console doctrine:schema:update --force
+
+Lancer le serveur :
+> php bin/console server:run
+
+Lancer le site :
+>http://localhost:8080/home
+
+Liste des routes disponibles : 
+	
+
+
+>home                                                  GET|POST   ANY      ANY    /home
+add                                                           ANY        ANY      ANY    /add
+fos_user_security_login                        GET|POST   ANY      ANY    /login
+fos_user_security_logout                      GET|POST   ANY      ANY    /logout
+fos_user_profile_show                               GET        ANY      ANY    /profile/
+fos_user_profile_edit                             GET|POST   ANY      ANY    /profile/edit
+fos_user_registration_register               GET|POST   ANY      ANY    /register/
+fos_user_registration_check_email            GET        ANY      ANY    /register/check-email
+fos_user_resetting_send_email                POST       ANY      ANY    /resetting/send-email
+fos_user_resetting_check_email                GET        ANY      ANY    /resetting/check-email
+fos_user_resetting_reset                       GET|POST   ANY      ANY    /resetting/reset/{token}
+fos_user_change_password                  GET|POST   ANY      ANY    /profile/change-password
+onePost                                                    ANY        ANY      ANY    /post/{idPost}
+post_like                                                  ANY        ANY      ANY    /post/like/{idPost}
+post_unlike                                              ANY        ANY      ANY    /post/unlike/{idPost}
+getPostApi                                               GET        ANY      ANY    /api/post/{idPost}
+getAllPostApi                                           GET        ANY      ANY    /api/home
+addCommentApi                                    POST        ANY      ANY    /api/post/{idPost}/addComment
--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -4,13 +4,29 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Post;
 use AppBundle\Form\PostType;
-
+use Datetime;
+use AppBundle\Form\CommentType;
+use AppBundle\Entity\Comment;
+use AppBundle\Entity\Vote;
 
 class PostController extends Controller
 {
+
+    public function getHomeAction(Request $request)
+    {
+        $posts = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findAll();
+
+
+        return $this->render('default/home.html.twig', array(
+            'posts' => $posts,
+        ));
+    }
 
     public function addPostAction(Request $request)
     {
@@ -49,5 +65,191 @@ class PostController extends Controller
             'form' => $form->createView(),
         ));
     }
+
+
+    public function addCommentAction($idPost, Request $request)
+    {
+
+        $post = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findOneById($idPost);
+        // 1) build the form
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setDate(new DateTime());
+            $comment->setUser($this->getUser());
+            $comment->setPost($post);
+
+            $tabComments = $post->getComments();
+            if($tabComments == null){
+                $tabComments = [];
+            }
+            array_push($tabComments, $comment);
+            $post->setComments($tabComments);
+
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+        }
+
+        return $this->render('default/onePost.html.twig', array(
+            'form' => $form->createView(),
+            'post' => $post,
+        ));
+    }
+
+    public function addLikeAction($idPost, Request $request){
+        $post = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findOneById($idPost);
+
+        $post->addVoteMoins();
+
+        $existingVote = false;
+        foreach ($post->getVote() as $key => &$value) {
+            if($value->getUser() == $this->getUser() && $value->getPost()->getId() == $idPost){
+                $value->setVote(true);
+                $existingVote = true;
+            }
+        }
+
+        if(!$existingVote){
+            $vote = new Vote();
+            $vote->setUser($this->getUser());
+            $vote->setVote(true);
+            $vote->setPost($post);
+            $totalVotes[] = $post->getVote();
+            array_push($totalVotes,$vote);
+            $post->setVote($totalVotes);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($vote);
+            $em->flush();
+        }
+
+
+    }
+
+    public function addUnlikeAction($idPost, Request $request){
+        $post = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findOneById($idPost);
+
+        $post->addVoteMoins();
+        $existingVote = false;
+        foreach ($post->getVote() as $key => &$value) {
+            if($value->getUser() == $this->getUser() && $value->getPost()->getId() == $idPost){
+                $value->setVote(false);
+                $existingVote = true;
+            }
+        }
+
+        if(!$existingVote){
+            $vote = new Vote();
+            $vote->setUser($this->getUser());
+            $vote->setVote(false);
+            $vote->setPost($post);
+            array_push($post->getVote(), $vote);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($vote);
+            $em->flush();
+        }
+    }
+
+
+    public function getPostApiAction($idPost, Request $request)
+    {
+
+        $post = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findOneById($idPost);
+
+        $thePost[] = [
+            'id' => $post->getId(),
+            'user' => $post->getUser(),
+            'image' => $post->getImage(),
+            'votePlus' => $post->getVotePlus(),
+            'voteMoins' => $post->getVoteMoins(),
+            'title' => $post->getTitle(),
+            'comments' => $post->getComments(),
+            'vote' => $post->getVote()
+        ];
+
+        return new JsonResponse($thePost);
+    }
+
+    public function getAllPostApiAction( Request $request)
+    {
+
+        $posts = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findAll();
+
+        foreach ($posts as $key=>  $post){
+            $thePost[$post->getId()] = [
+                'id' => $post->getId(),
+                'user' => $post->getUser(),
+                'image' => $post->getImage(),
+                'votePlus' => $post->getVotePlus(),
+                'voteMoins' => $post->getVoteMoins(),
+                'title' => $post->getTitle(),
+                'comments' => $post->getComments(),
+                'vote' => $post->getVote()
+            ];
+        }
+
+
+
+        return new JsonResponse($thePost);
+    }
+
+    public function addCommentApiAction($idPost, Request $request)
+    {
+
+        $post = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findOneById($idPost);
+        // 1) build the form
+        $comment = new Comment();
+
+        $comment->setDate(new DateTime());
+        $comment->setUser($this->getUser());
+        $comment->setPost($post);
+        $data = $request->get('comment');
+        $comment->setText($data);
+
+
+        $tabComments = $post->getComments();
+        if($tabComments == null){
+            $tabComments = [];
+        }
+        array_push($tabComments, $comment);
+        $post->setComments($tabComments);
+
+
+        // 4) save the User!
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($comment);
+        $em->flush();
+
+        $thePost[] = [
+            'id' => $post->getId(),
+            'user' => $post->getUser(),
+            'image' => $post->getImage(),
+            'votePlus' => $post->getVotePlus(),
+            'voteMoins' => $post->getVoteMoins(),
+            'title' => $post->getTitle(),
+            'comments' => $post->getComments(),
+            'vote' => $post->getVote()
+        ];
+
+        return new JsonResponse($thePost);
+    }
+
 
 }
